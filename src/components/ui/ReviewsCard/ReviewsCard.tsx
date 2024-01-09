be8578447eaf3c476430b38c6a8ce92f6a265704
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-01-09 17:09:20 +0200

    add right color text in reviewCard

--- src/components/ui/ReviewsCard/ReviewsCard.tsx
+++ src/components/ui/ReviewsCard/ReviewsCard.tsx
@@ -2,7 +2,6 @@ import User from '~/icons/user.svg';
 
 import { PropsReviews } from './types';
 
-//change color of title on #222
 export const ReviewsCard: React.FC<PropsReviews> = ({
   className = '',
   review,
@@ -11,7 +10,7 @@ export const ReviewsCard: React.FC<PropsReviews> = ({
     <li
       className={`flex h-[229px] w-[320px] flex-col justify-between rounded-3xl bg-color-bg-extra p-6 odd:bg-color-bg-primary md:h-[206px] md:w-[346px] xl:h-[294px] xl:w-[388px] ${className}`}
     >
-      <h4 className="font-manrope text-[18px] font-medium leading-[1.3] tracking-[-0.18px] text-color-main xl:text-[24px] xl:leading-[1.4] xl:tracking-[-0.24px]">
+      <h4 className="font-manrope text-[18px] font-medium leading-[1.3] tracking-[-0.18px] text-color-primary xl:text-[24px] xl:leading-[1.4] xl:tracking-[-0.24px]">
         {review.review}
       </h4>
       <div className="flex gap-2">
