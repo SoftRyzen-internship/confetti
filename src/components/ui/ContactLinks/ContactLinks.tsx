70d7f7aac873c7e30663483dde3e583f96b6e85c
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-01-10 16:47:35 +0200

    fix contact Links

--- src/components/ui/ContactLinks/ContactLinks.tsx
+++ src/components/ui/ContactLinks/ContactLinks.tsx
@@ -10,31 +10,30 @@ import { ContactType } from '@/types';
 
 export const ContactLinks: React.FC<Props> = ({ className = '', location }) => {
   const { data } = useFetch('contact') as { data: ContactType[] };
-  if (!data || data.length === 0) {
-    return [];
-  }
-  const { phone, email } = data[0];
+
   return (
     <ul
       className={`flex flex-col items-center gap-4 font-manrope text-2xl font-medium tracking-[-0.24px] text-color-text-extra ${className}`}
     >
-      <li>
-        <a
-          href={`tel:${phone}`}
-          className="group inline-flex items-center justify-center gap-2 transition-all hover:text-color-accent-primary focus:text-color-accent-primary"
-        >
-          <Tel className="h-6 w-6" /> {formatPhoneNumber(phone)}
-        </a>
-      </li>
-
-      {location === 'contacts' && (
+      {data && (
         <li>
           <a
-            href={`mailto:${email}`}
+            href={`tel:${data[0]?.phone}`}
+            className="group inline-flex items-center justify-center gap-2 transition-all hover:text-color-accent-primary focus:text-color-accent-primary"
+          >
+            <Tel className="h-6 w-6" /> {formatPhoneNumber(data[0]?.phone)}
+          </a>
+        </li>
+      )}
+
+      {location === 'contacts' && data && (
+        <li>
+          <a
+            href={`mailto:${data[0]?.email}`}
             className="group inline-flex items-center justify-center gap-2 transition-all hover:text-color-accent-primary focus:text-color-accent-primary"
           >
             <Mail className="h-6 w-6" />
-            {email}
+            {data[0]?.email}
           </a>
         </li>
       )}
