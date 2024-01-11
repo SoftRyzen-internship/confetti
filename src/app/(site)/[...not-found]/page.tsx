946f19c9bc95e28c43425329c5a5166cd7939777
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-01-11 15:55:20 +0100

    upd routing

---
+++ src/app/(site)/[...not-found]/page.tsx
@@ -0,0 +1,9 @@
+const NotFound = async () => {
+  return (
+    <section className="flex h-full grow items-center justify-center bg-red-50">
+      404
+    </section>
+  );
+};
+
+export default NotFound;
