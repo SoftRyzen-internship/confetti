7b06e46244a4df52483c5f23aaa356f8db82157e
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-11 12:24:48 +0200

    add footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,10 +1,27 @@
 import { Logo } from '@/components/ui';
 
+import data from '@/data/footer.json';
+
 export const Footer: React.FC = () => {
+  const { rights, link } = data;
+  const { label, href } = link;
   return (
-    <footer className="border-t-[1px] py-6">
+    <footer>
       <div className="container">
-        <Logo location="footer" />
+        <div className="footerLine relative flex flex-col items-center py-[32px] md:flex-row md:justify-between mdOnly:pb-[29px] mdOnly:pt-[25px]">
+          <Logo location="footer" />
+          <p className="font-manrope text-[16px] leading-[1.36] tracking-[-0.16px] text-color-primary  smOnly:mb-[8px] smOnly:mt-[16px]">
+            {rights}
+          </p>
+          <a
+            className="font-manrope text-[16px] leading-[1.36] tracking-[-0.16px] text-color-primary transition-all hover:text-color-accent-primary focus:text-color-accent-primary"
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {label}
+          </a>
+        </div>
       </div>
     </footer>
   );
