16c1c50dcaeeffcce85d7ec7695e858c431980d0
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-11 13:12:22 +0200

    fix mistakes

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,18 +1,23 @@
 import { Logo } from '@/components/ui';
 
-import data from '@/data/footer.json';
+import data from '@/data/common.json';
 
 export const Footer: React.FC = () => {
-  const { rights, link } = data;
+  const {
+    layout: { footer },
+  } = data;
+  const { rights, link } = footer;
   const { label, href } = link;
   return (
     <footer>
       <div className="container">
         <div className="footerLine relative flex flex-col items-center py-[32px] md:flex-row md:justify-between mdOnly:pb-[29px] mdOnly:pt-[25px]">
-          <Logo location="footer" />
-          <p className="font-manrope text-[16px] leading-[1.36] tracking-[-0.16px] text-color-primary  smOnly:mb-[8px] smOnly:mt-[16px]">
+          <Logo className="smOnly:mb-[16px]" location="footer" />
+
+          <p className="font-manrope text-[16px] leading-[1.36] tracking-[-0.16px] text-color-primary smOnly:mb-[8px]">
             {rights}
           </p>
+
           <a
             className="font-manrope text-[16px] leading-[1.36] tracking-[-0.16px] text-color-primary transition-all hover:text-color-accent-primary focus:text-color-accent-primary"
             href={href}
