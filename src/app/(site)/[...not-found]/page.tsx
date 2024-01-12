f661803c048a674dd62339d8c53c7869aa5e361d
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-01-12 13:29:13 +0200

    filled the not-found page with content , fix review card tag

--- src/app/(site)/[...not-found]/page.tsx
+++ src/app/(site)/[...not-found]/page.tsx
@@ -1,7 +1,41 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import balloons from '~/images/hero/left-balloons.webp';
+
+import common from '@/data/common.json';
+
 const NotFound = async () => {
+  const { title, button, img } = common.sections.notFound;
   return (
-    <section className="flex h-full grow items-center justify-center bg-red-50">
-      404
+    <section
+      className={`notFound flex h-full grow items-center justify-center`}
+    >
+      <div className="container relative flex flex-col items-center">
+        <div className="relative z-10 flex flex-col items-center">
+          <h2 className="mb-2 font-gilroy text-7xl font-extrabold leading-[1.2] tracking-[-0.72px] text-color-accent-primary md:text-9xl">
+            404
+          </h2>
+          <p className="mb-6 w-[320px] text-center font-manrope text-[16px] font-medium tracking-[-0.16px] text-color-text-primary md:w-[420px] md:text-xl">
+            {title}
+          </p>
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="rounded-3xl bg-color-accent-primary px-7 py-4 font-gilroy text-xl font-extrabold text-color-secondary transition-all hover:bg-color-accent-primary/90 md:px-8 md:py-5 md:text-2xl"
+            >
+              {button}
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={balloons}
+          width={700}
+          height={700}
+          alt={img}
+          className="balloonsUp"
+        />
+      </div>
     </section>
   );
 };
--- src/components/ui/ReviewsCard/ReviewsCard.tsx
+++ src/components/ui/ReviewsCard/ReviewsCard.tsx
@@ -10,9 +10,9 @@ export const ReviewsCard: React.FC<PropsReviews> = ({
     <div
       className={`flex h-[229px] w-[320px] flex-col justify-between rounded-3xl bg-color-bg-extra p-6 odd:bg-color-bg-primary md:h-[206px] md:w-[346px] xl:h-[294px] xl:w-[388px] ${className}`}
     >
-      <h4 className="font-manrope text-[18px] font-medium leading-[1.3] tracking-[-0.18px] text-color-primary xl:text-[24px] xl:leading-[1.4] xl:tracking-[-0.24px]">
+      <p className="font-manrope text-[18px] font-medium leading-[1.3] tracking-[-0.18px] text-color-primary xl:text-[24px] xl:leading-[1.4] xl:tracking-[-0.24px]">
         {card?.review}
-      </h4>
+      </p>
       <div className="flex gap-2">
         <User className="h-6 w-6" />
         <p className="font-manrope text-base font-medium tracking-[-0.16px] text-color-text-extra">
