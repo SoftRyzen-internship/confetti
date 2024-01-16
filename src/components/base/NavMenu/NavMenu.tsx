1a7bafb4c53d4f3893912f7434da39454fef965f
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-01-16 17:33:15 +0200

    adds closing menu by click on logo

--- src/components/base/NavMenu/NavMenu.tsx
+++ src/components/base/NavMenu/NavMenu.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 
 import { HomeNav, ServicesNav } from '@/components/base';
 
+import { Logo } from '@/components/ui';
+
 import { Props } from './types';
 
 export const NavMenu: React.FC<Props> = ({ toggleMenu }) => {
@@ -23,7 +25,15 @@ export const NavMenu: React.FC<Props> = ({ toggleMenu }) => {
 
   return (
     <div className="fixed left-0 top-0 z-40 h-full w-full items-center bg-color-secondary pb-[20px] pt-[108px] font-manrope md:flex md:justify-center md:pb-[32px] md:pt-[184px] xl:pt-[221px]">
-      <div className="flex h-full flex-col gap-6 overflow-hidden overflow-y-auto md:flex-row md:items-start md:justify-center md:gap-[190px] xl:gap-[352px]">
+      <div className="smOnly:container smOnly:relative">
+        <Logo
+          location="header"
+          onClick={toggleMenu}
+          className="absolute -top-[72px] right-5 md:right-[50%] md:top-8 md:translate-x-[50%] xl:top-[48px]"
+        />
+      </div>
+
+      <div className="container flex h-full flex-col gap-6 overflow-hidden overflow-y-auto md:flex-row md:items-start md:justify-center md:gap-[190px] xl:gap-[352px] smOnly:relative ">
         <HomeNav toggleMenu={toggleMenu} />
         <ServicesNav toggleMenu={toggleMenu} />
       </div>
