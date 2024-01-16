87078ea25a7cd8e915a3f1ece61b740bca48b067
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-01-16 15:30:39 +0200

    Fixes bugs in menu button and contacts section

--- src/components/base/NavMenu/types.ts
+++ src/components/base/NavMenu/types.ts
@@ -1,5 +1,3 @@
-import { Dispatch, SetStateAction } from 'react';
-
 export type Props = {
-  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+  toggleMenu: () => void;
 };
--- src/components/base/ServicesNav/ServicesNav.tsx
+++ src/components/base/ServicesNav/ServicesNav.tsx
@@ -8,29 +8,19 @@ const {
   services: { servicesTitle, navLinks },
 } = data.layout.navigation;
 
-export const ServicesNav: React.FC<Props> = ({ closeMenu }) => {
-  // Оставил, стили для варианта, где скорллится только услуги, чтобы если что, вернуть.
-  // Переделываю потому, что такой вариант выглядит некоректно на мобилбилках в горизонтальном режиме
+export const ServicesNav: React.FC<Props> = ({ toggleMenu }) => {
   return (
-    <div
-    // className="flex grow flex-col gap-4 overflow-y-scroll smOnly:pb-9"
-    >
-      <p
-        // className="text-center text-2xl/normal font-medium -tracking-[0.01em] md:text-start md:text-4xl/normal"
-        className="mb-4 text-center text-2xl/normal font-medium -tracking-[0.01em] md:text-start md:text-4xl/normal"
-      >
+    <div>
+      <p className="mb-4 text-center text-2xl/normal font-medium -tracking-[0.01em] md:text-start md:text-4xl/normal">
         {servicesTitle}
       </p>
 
-      <ul
-        // className="flex flex-col items-center gap-3 overflow-scroll md:items-start md:gap-4"
-        className="flex flex-col items-center gap-3 md:mt-6 md:items-start md:gap-4"
-      >
+      <ul className="flex flex-col items-center gap-3 md:mt-6 md:items-start md:gap-4">
         {navLinks.map(({ slug, label }) => (
           <li key={slug}>
             <NextLink
               className="font-medium -tracking-[0.01em] transition-all hover:text-color-accent-primary focus:text-color-accent-primary md:text-2xl/normal"
-              onClick={closeMenu}
+              onClick={toggleMenu}
               href={`/${slug}`}
             >
               {label}
