3baa2dd4a88585f0d2c20464b77e36201aa6a4cc
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-01-18 13:39:42 +0100

    upd slider controls

--- src/components/ui/SliderControls/SliderControls.tsx
+++ src/components/ui/SliderControls/SliderControls.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import NextIcon from '~/icons/arrow-right.svg';
+import { MouseEvent } from 'react';
 
 import common from '@/data/common.json';
+
+import NextIcon from '~/icons/arrow-right.svg';
 
 import { SliderControlsProps } from './types';
 
@@ -20,6 +22,9 @@ export const SliderControls: React.FC<SliderControlsProps> = ({ section }) => {
         aria-label={prevBtnArialabel}
         className={`button-prev-${section} flex h-14 w-14 items-center justify-center rounded-full border border-solid border-color-ctrl-btn-border bg-color-secondary transition-all hover:border-transparent hover:bg-color-bg-primary focus:border-transparent focus:bg-color-bg-primary`}
         type="button"
+        onClick={(e: MouseEvent<HTMLButtonElement>) => {
+          e.currentTarget.blur();
+        }}
       >
         <NextIcon
           className="rotate-180"
@@ -33,6 +38,9 @@ export const SliderControls: React.FC<SliderControlsProps> = ({ section }) => {
         aria-label={nextBtnArialabel}
         className={`button-next-${section} flex h-14 w-14 items-center justify-center rounded-full border border-solid border-color-ctrl-btn-border bg-color-secondary transition-all hover:border-transparent hover:bg-color-bg-primary focus:border-transparent focus:bg-color-bg-primary`}
         type="button"
+        onClick={(e: MouseEvent<HTMLButtonElement>) => {
+          e.currentTarget.blur();
+        }}
       >
         <NextIcon aria-label={nextBtnArialabelIcon} width={24} height={24} />
       </button>
