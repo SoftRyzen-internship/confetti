208d944337ba5a9eb08455370fc413a319a1fabf
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-01-19 13:15:25 +0200

    fixes bugs

--- src/components/ui/MenuBtn/types.ts
+++ src/components/ui/MenuBtn/types.ts
@@ -1,7 +1,4 @@
-import { MouseEvent } from 'react';
-
 export type Props = {
   isMenuOpen: boolean;
-  // eslint-disable-next-line no-unused-vars
-  toggleMenu: (e: MouseEvent<HTMLButtonElement>) => void;
+  toggleMenu: () => void;
 };
